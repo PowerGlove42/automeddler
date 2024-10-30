b1abbe030a36ba33a68455226eb8326f45089f19
PowerGlove42 <nikas.g.ng@gmail.com>
2024-10-30 08:48:21 +0100

    restructured project

---
+++ control/script/shop.gd
@@ -0,0 +1,36 @@
+extends Control
+
+@export var anchor:Node;
+#var units = [];
+const juan = "res://control/scene/spawner_juan.tscn"
+const dog = "res://control/scene/spawner_dog.tscn"
+const based = "res://control/scene/spawner_based.tscn"
+const lancerlot = "res://control/scene/spawner_lancerlot.tscn"
+@onready var hbox = $HBoxContainer;
+
+func _ready() -> void:
+	if not anchor:
+		anchor = get_parent();
+	for i in range(0, 5, 1):
+		var rand = randi_range(0, 3);
+		var offer;
+		var offer_button
+		match rand:
+			0:
+				offer = load(juan);  
+			1:
+				offer = load(dog);
+			2:
+				offer = load(based); 
+			3:
+				offer = load(lancerlot);
+		offer_button = offer.instantiate()
+		offer_button.anchor = anchor;
+		hbox.add_child(offer_button);
+
+
+
+func _on_button_button_down() -> void:
+	for i in get_tree().get_nodes_in_group("remove_on_shop_refresh"):
+		i.queue_free();
+	_ready();
---
+++ control/script/spawner.gd
@@ -0,0 +1,16 @@
+extends Node
+
+@export_file var path;
+@export var anchor:Node
+var entity;
+
+func _on_button_down() -> void:
+	spawn();
+
+func spawn():
+	entity = load(path);
+	if not anchor:
+		anchor = get_parent();
+	var new = entity.instantiate();
+	anchor.add_child(new);
+	call_deferred("queue_free");
